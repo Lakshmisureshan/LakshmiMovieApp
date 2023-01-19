b717a4f603ef130ef4b3f8461a51f64d800b2b23
Lakshmi Sureshan <lakshmisureshan@gmail.com>
2023-01-19 05:45:42 +0400

    Added Appbdcontext

---
+++ LakshmiMovieApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LakshmiMovieApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LakshmiMovieApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options ):base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
